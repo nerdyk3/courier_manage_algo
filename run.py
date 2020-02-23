5b746547b02c94e9eac22edf94086469ccccfc3b
kk <kunalkumar.kus@gmail.com>
2020-02-23 23:22:36 +0530

    json file added

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import requests
 from flask import request, Flask
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -12,22 +13,33 @@ def home():
 
 @app.route('/add',methods=['POST','GET'])
 def add_carrier():
+    """{
+        "varicle_minimum_gurantee": "2400", 
+        "varicle_monthly_cycle_end": "2/10/2019", 
+        "varicle_monthly_cycle_start": "1/10/2019", 
+        "varicle_number": "12", 
+        "varicle_rate": "20", 
+        "varicle_type": "v1"
+        }"""
     varicle_number = input("Input varicle_number:")
     varicle_type = input("Input varicle_type(v1,v2,v3,v4,v5):")
-    varicle_rate = input("Input varicle_number:")
-    varicle_monthly_cycle_start = input("Input monthly_cycle_start:")
-    varicle_monthly_cycle_end = input("Input varicle_monthly_cycle_end:")
-    varicle_minimum_guranteee = input("Input varicle_minimum_guranteee:")
+    varicle_rate = input("Input varicle_rate / km:")
+    varicle_monthly_cycle_start = input("Input monthly_cycle_start(DD/MM):")
+    varicle_monthly_cycle_end = input("Input varicle_monthly_cycle_end(DD/MM):")
+    varicle_minimum_gurantee = input("Input varicle_minimum_gurantee/km:")
     context ={
         "varicle_number":varicle_number,
         "varicle_type":varicle_type,
         "varicle_rate":varicle_rate,
         "varicle_monthly_cycle_start":varicle_monthly_cycle_start,
         "varicle_monthly_cycle_end":varicle_monthly_cycle_end,
-        "varicle_minimum_guranteee":varicle_minimum_guranteee
+        "varicle_minimum_gurantee":varicle_minimum_gurantee
     }
+    with open('./data.json', 'a') as outfile:
+        json.dump(context, outfile)
     return context
 
+@app.route('/calculation',methods=['POST','GET'])
 def rate_calculation():
     return "pass"
 
